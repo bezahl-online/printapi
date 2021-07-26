2ddbf046421c0860016f155247ff4f7730d6cfbd
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-07-26 17:58:20 +0200

    add env INVOICE_PDF_URL

--- api/GetInvoice.go
+++ api/GetInvoice.go
@@ -3,12 +3,21 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
+const INVOICE_PDF_URL = "INVOICE_PDF_URL"
+
 func (a *API) GetInvoice(ctx echo.Context, params GetInvoiceParams) error {
-	uri := "http://localhost:8090/invoice_pdf?code=" + params.Code
+	url := "http://localhost:8090/invoice_pdf?code="
+	if len(os.Getenv(INVOICE_PDF_URL)) > 0 {
+		url = os.Getenv(INVOICE_PDF_URL)
+	} else {
+		fmt.Printf("please set environment variable '%s'", INVOICE_PDF_URL)
+	}
+	uri := url + params.Code
 
 	// Get the data
 	resp, err := http.Get(uri)
